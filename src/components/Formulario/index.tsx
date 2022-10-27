4d22d989f32990540b401b7f5d4de13253d16660
Lucas Sales <lucas.salles.oliveira@gmail.com>
2022-10-27 19:00:25 -0300

    Primeiros passos do projeto

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Botao from '../Botao'
+
+export default function Formulario() {
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="">
+
+                </label>
+                <input type="text" name="tarefa" id="tarefa" placeholder='O que você quer estudar' required />
+            </div>
+        </form>
+    )
+}
